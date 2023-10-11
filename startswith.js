c0b15b46aca392b020e304fc36867690d91e8e0b
sumit <sumitmessi018@gmail.com>
2023-10-11 20:02:43 +0530

    champions

---
+++ startswith.js
@@ -0,0 +1,17 @@
+const str1 = 'Saturday night plans';
+
+console.log(str1.startsWith('Sat'));
+
+console.log(str1.startsWith('Sat', 3));
+
+const str2 = 'today india is going to win';
+
+console.log(str2.startsWith('tod'));
+
+console.log(str2.startsWith('tod', 3));
+
+const str3 = 'virat is the king';
+
+console.log(str3.startsWith('virat'));
+
+console.log(str3.startsWith('virat', 5));
